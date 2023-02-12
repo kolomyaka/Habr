266bbd68cde60ecc157e1a34e61b2b8c9eaffb5e
Nikita <66018338+kolomyaka1@users.noreply.github.com>
2023-02-12 22:16:29 +0300

    downgrade husky

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -10,7 +10,7 @@ export function classNames(cls: string, mods: Mods = {}, additional: string[] = 
 
     return [
         cls
-        ...additional.filter(Boolean),
+        ...additional.filter(Boolean)
         ...Object.entries(mods)
             .filter(([_, value]) => Boolean(value))
             .map(([className]) => className)
