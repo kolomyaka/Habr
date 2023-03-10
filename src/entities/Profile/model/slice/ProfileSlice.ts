22ad7615828141b506a0f8d8aeae4c8fcab1a5d5
Nikita <66018338+kolomyaka1@users.noreply.github.com>
2023-03-10 21:26:37 +0300

    fix import

--- src/entities/Profile/model/slice/ProfileSlice.ts
+++ src/entities/Profile/model/slice/ProfileSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { ProfileSchema } from '../types/Profile';
+import { ProfileSchema } from '../types/profile';
 
 const initialState: ProfileSchema = {
     data: undefined,
