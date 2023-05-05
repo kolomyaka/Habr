412b68488325761da1dd99807d2538149e6a888c
Nikita <66018338+kolomyaka1@users.noreply.github.com>
2023-05-05 19:25:00 +0300

    Добавил плагин для ордера импортов и правила импортов для структуры FSD.

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,10 @@ module.exports = {
         'plugin:react/recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:i18next/recommended',
+        'plugin:boundaries/recommended',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:import/typescript',
     ],
     'overrides': [
         {
@@ -39,9 +43,74 @@ module.exports = {
     'settings': {
         'react': {
             'version': 'detect'
+        },
+        'boundaries/elements': [
+            { type: 'app', pattern: 'app/*' },
+            { type: 'pages', pattern: 'pages/*' },
+            { type: 'widgets', pattern: 'widgets/*' },
+            { type: 'features', pattern: 'features/*' },
+            { type: 'entities', pattern: 'entities/*' },
+            { type: 'shared', pattern: 'shared/*' }
+        ],
+        'boundaries/ignore': ['**/*.test.*'],
+        'import/resolver': {
+            'typescript': {
+                'alwaysTryTypes': true
+            }
         }
     },
     'rules': {
+        'boundaries/element-types': [
+            'warn',
+            {
+                default: 'disallow',
+                rules: [
+                    { from: 'app', allow: ['pages', 'widgets', 'features', 'entities', 'shared'] },
+                    { from: 'pages', allow: ['widgets', 'features', 'entities', 'shared'] },
+                    { from: 'widgets', allow: ['features', 'entities', 'shared'] },
+                    { from: 'features', allow: ['entities', 'shared'] },
+                    { from: 'entities', allow: ['shared'] },
+                    { from: 'shared', allow: ['shared'] }
+                ]
+            }
+        ],
+        'no-restricted-imports': [
+            'error',
+            {
+                patterns: [
+                    { message: 'Private imports are prohibited, use public imports instead', group: ['@/app/*/*/**'] },
+                    { message: 'Private imports are prohibited, use public imports instead', group: ['@/pages/*/**'] },
+                    { message: 'Private imports are prohibited, use public imports instead', group: ['@/widgets/*/**'] },
+                    { message: 'Private imports are prohibited, use public imports instead', group: ['@/features/*/**'] },
+                    { message: 'Private imports are prohibited, use public imports instead', group: ['@/entities/*/**'] },
+                    { message: 'Private imports are prohibited, use public imports instead', group: ['@/shared/*/*/*/**'] },
+                    { message: 'Prefer absolute imports instead of relatives (for root modules)', group: ['../**/app'] },
+                    { message: 'Prefer absolute imports instead of relatives (for root modules)', group: ['../**/processes'] },
+                    { message: 'Prefer absolute imports instead of relatives (for root modules)', group: ['../**/pages'] },
+                    { message: 'Prefer absolute imports instead of relatives (for root modules)', group: ['../**/widgets'] },
+                    { message: 'Prefer absolute imports instead of relatives (for root modules)', group: ['../**/features'] },
+                    { message: 'Prefer absolute imports instead of relatives (for root modules)', group: ['../**/entities'] },
+                    { message: 'Prefer absolute imports instead of relatives (for root modules)', group: ['../**/shared'] }
+                ]
+            }
+        ],
+        'import/order': [
+            'error',
+            {
+                alphabetize: { order: 'asc', caseInsensitive: true },
+                'newlines-between': 'always',
+                pathGroups: [
+                    { group: 'internal', position: 'after', pattern: '@/pages/**' },
+                    { group: 'internal', position: 'after', pattern: '@/widgets/**' },
+                    { group: 'internal', position: 'after', pattern: '@/features/**' },
+                    { group: 'internal', position: 'after', pattern: '@/entities/**' },
+                    { group: 'internal', position: 'after', pattern: '@/shared/**' }
+                ],
+                pathGroupsExcludedImportTypes: ['builtin'],
+                groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index']
+            }
+        ],
+
 
         'react/jsx-indent': [2, 4, { indentLogicalExpressions: true }],
         indent: [2,4],
@@ -62,7 +131,7 @@ module.exports = {
                     ['to', 'data-testid', 'name', 'target', 'align', 'justify', 'gap', 'direction', 'as']
             }
         ],
-        'max-len': ['error', { ignoreComments: true, code: 120 }],
+        'max-len': ['error', { ignoreComments: true, code: 130 }],
         'linebreak-style': [
             'error',
             'unix'
