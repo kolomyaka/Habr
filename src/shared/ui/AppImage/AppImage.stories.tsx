684ecda290da23bc604793a4cd3c0163dd1452fe
Nikita <66018338+kolomyaka1@users.noreply.github.com>
2023-06-08 00:24:42 +0300

    fix storybook

--- src/shared/ui/AppImage/AppImage.stories.tsx
+++ src/shared/ui/AppImage/AppImage.stories.tsx
@@ -1,6 +1,8 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import React from 'react';
 
+import { Skeleton } from '../Skeleton/Skeleton';
+
 import { AppImage } from './AppImage';
 
 export default {
@@ -15,7 +17,13 @@ export default {
 const Template: ComponentStory<typeof AppImage> = (args) => <AppImage {...args} />;
 
 export const Primary = Template.bind({});
-Primary.args = {};
+Primary.args = {
+    width: 'auto',
+    height: 250,
+    src: 'https://i.ytimg.com/vi/tXPUueXzmG8/maxresdefault.jpg',
+    fallback: <Skeleton width={150} height={150} />,
+    errorFallback: <Skeleton width={150} height={150} />
+};
 
 
 
